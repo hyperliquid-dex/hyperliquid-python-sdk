2e9f1ce38dfc04f1e89f45a0ff5c8138b0aefa52
Louis Tao <louis@chameleontrading.com>
2024-02-15 18:22:54 -0500

    Version 0.2.0

---
+++ examples/basic_set_referrer.py
@@ -0,0 +1,17 @@
+from hyperliquid.utils import constants
+import example_utils
+
+
+def main():
+    address, info, exchange = example_utils.setup(constants.TESTNET_API_URL, skip_ws=True)
+
+    # Set the referrer code, for non-subaccount and non-vault addresses
+    print(exchange.set_referrer("ASDFASDF"))
+
+    referral_state = info.query_referral_state(address)
+    if "referredBy" in referral_state:
+        print("referred by", referral_state["referredBy"])
+
+
+if __name__ == "__main__":
+    main()
