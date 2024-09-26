1555fd695929734d2e8506bd160a67e37fbeef9c
Lox3ur <33488209+cgaspart@users.noreply.github.com>
2024-09-26 11:10:18 +0200

    Update examples/basic_vault_transfer.py
    Co-authored-by: Ben <ben@hl-labs.xyz>

--- examples/basic_vault_transfer.py
+++ examples/basic_vault_transfer.py
@@ -9,7 +9,7 @@ def main():
         raise Exception("Agents do not have permission to perform internal transfers")
 
     # Transfer 5 usd to the HLP Vault for demonstration purposes
-    transfer_result = exchange.vault_usd_transfer("0xa15099a30bbf2e68942d6f4c43d70d04faeab0a0", True, 5000000)
+    transfer_result = exchange.vault_usd_transfer("0xa15099a30bbf2e68942d6f4c43d70d04faeab0a0", True, 5_000_000)
     print(transfer_result)
 
 
