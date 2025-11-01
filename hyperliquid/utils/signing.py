2022e15fffd542075879ab871885ac34e7d05a0c
vivekascoder <vivekascoder@gmail.com>
2025-11-01 12:16:12 +0530

    feat(signing): add type hint for order_wires_to_order_action function

--- hyperliquid/utils/signing.py
+++ hyperliquid/utils/signing.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import time
 from decimal import Decimal
 
@@ -496,7 +498,7 @@ def order_request_to_order_wire(order: OrderRequest, asset: int) -> OrderWire:
     return order_wire
 
 
-def order_wires_to_order_action(order_wires, builder=None, grouping: Grouping = "na"):
+def order_wires_to_order_action(order_wires, builder=None, grouping: Grouping = "na") -> Any:
     action = {
         "type": "order",
         "orders": order_wires,
